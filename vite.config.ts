6e2cf81df28ca9aadb5259029a7701f93fd4b8fa
unknown <joaopedrodacunha.santos@gmail.com>
2026-02-04 15:23:16 -0300

    init vite-sitemap

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
+import Sitemap from 'vite-plugin-sitemap'
 import react from '@vitejs/plugin-react';
+
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
@@ -9,7 +11,9 @@ export default defineConfig(({ mode }) => {
         port: 3000,
         host: '0.0.0.0',
       },
-      plugins: [react()],
+      plugins: [react(), Sitemap({
+        hostname: 'https://ebookrendaextra.com.br/',
+      })],
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
